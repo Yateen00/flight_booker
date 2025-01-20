d1675f1ae521ede8cde06e0a5d95557dfea1f0ac
Yateen00 <codingdudelolhow@gmail.com>
2025-01-20 23:58:48 +0530

    created seed files

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,16 +1,30 @@
+# not checking overlapping flights
 class Flight < ApplicationRecord
-  belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_airport_id", inverse_of: "departing_flights"
-  belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport_id", inverse_of: "arriving_flights"
+  belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_airport_id",
+    inverse_of: "departing_flights"
+  belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport_id",
+    inverse_of: "arriving_flights"
 
   before_validation :capitalize_airports
 
-  validates :departure_airport_id, :arrival_airport_id, :departure_time, :arrival_time, presence: true
+
+  validates :departure_airport_id, :arrival_airport_id, :departure_time, :arrival_time,
+    presence: true
   validates :departure_airport_id, :arrival_airport_id, length: { is: 3 }
-  validates :departure_airport_id, :arrival_airport_id, format: { with: /\A[A-Z]+\z/, message: "only allows uppercase letters" }
+  validates :departure_airport_id, :arrival_airport_id,
+    format: { with: /\A[A-Z]+\z/, message: "only allows uppercase letters" }
+  validate :arrival_time_after_departure_time
 
   private
     def capitalize_airports
       self.departure_airport_id = departure_airport_id.upcase if departure_airport_id.present?
       self.arrival_airport_id = arrival_airport_id.upcase if arrival_airport_id.present?
+    end
+    def arrival_time_after_departure_time
+      return unless departure_time.present? && arrival_time.present?
+
+      if arrival_time <= departure_time
+        errors.add(:arrival_time, "must be after the departure time")
+      end
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# airport name is all letters seperated by spaces
+ActiveRecord::Base.transaction do
+  CHARSET = ("A".."Z").to_a
+  airports = []
+
+  10.times do |i|
+    code = CHARSET.sample(3).join
+    unless Airport.exists?(code: code)
+      airport = Airport.create!(code: code, name: code.split("").join("-"))
+      airports << airport
+    end
+    p "created airport #{i + 1}"
+  end
+
+  rand = Random.new 1234
+  startt = Time.now
+  p "created airports"
+
+  20.times do |i|
+    start_airport = airports.sample
+    end_airport = airports.sample
+    Flight.create!(
+      departure_airport_id: start_airport.code,
+      arrival_airport_id: end_airport.code,
+      departure_time: startt,
+      arrival_time: startt + 2.days
+    )
+    startt += rand.rand(-10..10).hours
+    p "created flight #{i + 1}"
+  end
+
+  p "created flights"
+end
+p "seeded"
