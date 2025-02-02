7b60974477b82997533ab97e066b50ab238ec609
Yateen00 <codingdudelolhow@gmail.com>
2025-02-02 22:55:51 +0530

    added comment for alternative way

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,32 @@ class BookingsController < ApplicationController
     @booking = @flight.bookings.build
     number_of_passengers = params[:number].to_i
     number_of_passengers.times { @booking.passengers.build }
+    # alt: @number = params[:number].to_i
+    # then in _form:
+    # <h3>Passengers</h3>
+    # <div data-controller="passenger" data-passenger-initial-count="<%= @number %>">
+    #   <div data-passenger-target="passenger">
+    #     <!-- Initial passenger fields will be added here by JavaScript -->
+    #   </div>
+    #   <button data-action="click->passenger#addPassenger">Add passenger</button>
+
+    #   <template data-passenger-target="template">
+    #     <%= f.fields_for :passengers, Passenger.new, child_index: 'TEMPLATE_RECORD' do |p| %>
+    #       <%= render 'passenger_fields', f: p %>
+    #     <% end %>
+    #   </template>
+    # </div>
+    #   js:
+    # connect() {
+    #   const initialCount = this.element.dataset.passengerInitialCount;
+    #   for (let i = 0; i < initialCount; i++) {
+    #     this.addPassenger();
+    #   }
+    # }
+
+    # addPassenger(event) {
+    #   if (event) event.preventDefault();
+
   end
 
   def create
